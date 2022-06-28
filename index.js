84b89cf35585b4a5d6e457236def9c91de670a82
Nick Warren <nickwarren47@gmail.com>
2022-06-28 08:02:43 -0600

    Done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Write your solution in this file!
+var customerName = 'bob'
+
+function upperCaseCustomerName(){
+   return customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer(){
+  bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer(){
+    bestCustomer = 'maybe bob';
+}
+const leastFavoriteCustomer = 'tim'
+
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = 'jim'
+}
