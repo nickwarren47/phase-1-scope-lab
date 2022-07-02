0cd9f3d981b22e38d8f9ce5084d4dcbc47c719bd
Nick Warren <nickwarren47@gmail.com>
2022-07-01 21:25:01 -0600

    commit tree ef86f95db94565046c5cbb986d6bbecf2724ff06

--- index.js
+++ index.js
@@ -1,18 +1,41 @@
-var customerName = 'bob'
+// var customerName = 'bob'
 
-function upperCaseCustomerName(){
-   return customerName = customerName.toUpperCase();
-}
+// function upperCaseCustomerName(){
+//    return customerName = customerName.toUpperCase();
+// }
 
-function setBestCustomer(){
-  bestCustomer = 'not bob';
-}
+// function setBestCustomer(){
+//   bestCustomer = 'not bob';
+// }
+
+// function overwriteBestCustomer(){
+//     bestCustomer = 'maybe bob';
+// }
+// const leastFavoriteCustomer = 'tim'
+
+// function changeLeastFavoriteCustomer(){
+//     leastFavoriteCustomer = 'jim'
+// }
+
+
+var customerName = 'bob';
+
+function upperCaseCustomerName () {
+  return customerName = customerName.toUpperCase();
+};
+
+
+function setBestCustomer() {
+  return bestCustomer = 'not bob';
+};
 
 function overwriteBestCustomer(){
-    bestCustomer = 'maybe bob';
-}
+  return bestCustomer = 'maybe bob';
+};
+
 const leastFavoriteCustomer = 'tim'
 
 function changeLeastFavoriteCustomer(){
-    leastFavoriteCustomer = 'jim'
-}
+  return leastFavoriteCustomer = "bob's ok";
+};
+
